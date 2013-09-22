d844f73937016314a2174e42415748bb0762a558
Yann Bajot <yann.bajot@free.fr>
2013-09-22 23:42:32 +0200

    Update

--- BashScripts/BackupLancieuxToWestern.sh
+++ BashScripts/BackupLancieuxToWestern.sh
@@ -1,6 +1,6 @@
 echo "Starting Backup..."
 BACKUP_DIR=""
-CMD="rsync -dry-run --recursive --exclude=Thumbs.db --times --modify-window=1 --chmod=ugo=rwX --itemize-changes"
+CMD="rsync --dry-run --recursive --exclude=Thumbs.db --exclude=.git --times --modify-window=1 --chmod=ugo=rwX --itemize-changes"
 
 $CMD --exclude=Downloads --exclude=ASUS --exclude=CyberLink /cygdrive/d/MesDocuments /cygdrive/f/MesDocuments_saved/
 $CMD /cygdrive/d/Musique /cygdrive/f/MesDocuments_saved/
